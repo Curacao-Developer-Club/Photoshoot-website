2d10faa72ac8a560a86050057e3b53c0f6e85ff5
SAINT-RONIN <hidude1428@gmail.com>
2022-03-08 15:28:35 -0400

    upload

--- index.php
+++ index.php
@@ -4,48 +4,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="views/styles.css" rel="stylesheet"></src>
+    <title>I'm a Photographer</title>
+    <!-- font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
-    <title>Photography</title>
+    <link href="views/styles.css" rel="stylesheet"></src>
 </head>
 <body>
-    <!-- Header -->
-    <header id= "Header" class= "vh-100-flex">
-        <div class= "container">
-            <div class= "header-content">
-                <h1>My name is <br><span></span></h1>
-                <h3>Leandro Nuñez</h3>
+    
+    <!-- header -->
+    <header id = "header" class = "vh-100 flex">
+        <div class = "container">
+            <div class = "header-content">
+                <h1>My name is <br><span class = "typewrite" data-loop = "yes" data-speed = "100" data-delay = "2000" data-words = '["photographer", "videographer", "video editor"]'></span></h1>
+                <h3>Jason Miller</h3>
 
-                <ul class= "spcial-links">
+                <ul class = "social-links">
                     <li>
-                        <a href= "#" class= "flex">
-                            <i class= "fab fa-facebook-f"></i>
+                        <a href = "#" class = "flex">
+                            <i class = "fab fa-facebook-f"></i>
                         </a>
                     </li>
                     <li>
-                        <a href= "#" class= "flex">
-                            <i class= "fab fa-instagram"></i>
+                        <a href = "#" class = "flex">
+                            <i class = "fab fa-twitter"></i>
                         </a>
                     </li>
                     <li>
-                        <a href= "#" class= "flex">
-                            <i class= "fab fa-twitter"></i>
+                        <a href = "#" class = "flex">
+                            <i class = "fab fa-instagram"></i>
                         </a>
                     </li>
                     <li>
-                        <a href= "#" class= "flex">
-                            <i class= "fab fa-pinterest"></i>
+                        <a href = "#" class = "flex">
+                            <i class = "fab fa-pinterest"></i>
                         </a>
                     </li>
-                </ul>  
+                </ul>
             </div>
         </div>
-        
-    <!-- End of Header -->
+    </header>
+    <!-- end of header -->
 
-
-    <!---- Typewriting JS ----->
+    <!-- main content -->
     <script src="views/script.js"></script>
 </body>
 </html>
+
