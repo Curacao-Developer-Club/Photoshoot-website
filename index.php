f85810459c2494c56b37c7160e203a6bd07f1779
SAINT-RONIN <hidude1428@gmail.com>
2022-03-08 14:03:48 -0400

    update

--- index.php
+++ index.php
@@ -4,15 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="views/style.css" rel="stylesheet"></src>
+    <link href="views/styles.css" rel="stylesheet"></src>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
-    <title>Document</title>
+    <title>Photography</title>
 </head>
 <body>
-    
+    <!-- Header -->
+    <header id= "Header" class= "vh-100-flex">
+        <div class= "container">
+            <div class= "header-content">
+                <h1>My name is <br><span></span></h1>
+                <h3>Leandro Nuñez</h3>
+
+                <ul class= "spcial-links">
+                    <li>
+                        <a href= "#" class= "flex">
+                            <i class= "fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href= "#" class= "flex">
+                            <i class= "fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href= "#" class= "flex">
+                            <i class= "fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href= "#" class= "flex">
+                            <i class= "fab fa-pinterest"></i>
+                        </a>
+                    </li>
+                </ul>  
+            </div>
+        </div>
+        
+    <!-- End of Header -->
 
 
-
+    <!---- Typewriting JS ----->
     <script src="views/script.js"></script>
 </body>
 </html>
